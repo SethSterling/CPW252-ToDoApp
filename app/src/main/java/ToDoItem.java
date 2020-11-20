7dfc17eb835e861c1c87d9eb6dcd20ce9c9e3729
SethSterling <sethbdsterling@gmail.com>
2020-11-19 21:39:07 -0800

    Created ToDoItem Class

---
+++ app/src/main/java/ToDoItem.java
@@ -0,0 +1,27 @@
+public class ToDoItem {
+    private String Title;
+    private String DueDate;
+    private String Description;
+
+    public ToDoItem(String title, String dueDate, String description){
+        Title = title;
+        Description = description;
+        DueDate = dueDate;
+    }
+
+    public ToDoItem(String title, String dueDate){
+        this(title, dueDate, "No Description");
+    }
+
+    public String getTitle(){
+        return Title;
+    }
+
+    public String getDescription(){
+        return Description;
+    }
+
+    public String getDueDate(){
+        return DueDate;
+    }
+}
